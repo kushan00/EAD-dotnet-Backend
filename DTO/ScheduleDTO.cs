0d3f9cadf34c427c2f4a09af3fc74549331c2e4a
Chirath Jayawardena <88072810+IT20228576@users.noreply.github.com>
2023-10-08 12:16:05 +0530

    add schedule models

---
+++ DTO/ScheduleDTO.cs
@@ -0,0 +1,18 @@
+using backend.Models;
+
+namespace backend.DTO;
+    public class ScheduleDTO
+    {
+    public String? Id { get; set; }
+    public String? StartCity { get; set; } = null!;
+    public String[]? Cities { get; set; } = null!;
+    public String? EndCity { get; set; } = null!;
+    public int? Price { get; set; } = null!;
+    public Train? Train { get; set; } = null!;
+    public DateTime? StartTime { get; set; } = null!;
+    public DateTime? EndTime { get; set; } = null!;
+    public String? Class { get; set; } = null!;
+    public String? Type { get; set; } = null!;
+    public String? RunBy { get; set; } = null!;
+    public Boolean? IsActive { get; set; } = null!;
+}
---
+++ Models/Schedule.cs
@@ -0,0 +1,25 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace backend.Models;
+public class Schedule
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    [BsonElement("_id")]
+    public String? Id { get; set; }
+    public String? StartCity { get; set; } = null!;
+    public String[]? Cities { get; set; } = null!;
+    public String? EndCity { get; set; } = null!;
+    public int? Price { get; set; } = null!;
+    [BsonRepresentation(BsonType.ObjectId)]
+    public String? Train { get; set; } = null!;
+    public DateTime? StartTime { get; set; } = null!;
+    public DateTime? EndTime { get; set; } = null!;
+    public String? Class { get; set; } = null!;
+    public String? Type { get; set; } = null!;
+    public String? RunBy { get; set; } = null!;
+    public Boolean? IsActive { get; set; } = null!;
+    [BsonDateTimeOptions(Kind = DateTimeKind.Utc, Representation = BsonType.DateTime)]
+    public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+}
