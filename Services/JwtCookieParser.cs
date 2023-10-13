2aeaa322868c71666b7182319b63febf87e924bc
Chirath Jayawardena <88072810+IT20228576@users.noreply.github.com>
2023-10-13 19:52:58 +0530

    BUG FIX

--- Services/JwtCookieParser.cs
+++ Services/JwtCookieParser.cs
@@ -18,6 +18,15 @@ public class JwtCookieParser
         _next = next;
     }
 
+    /// <summary>
+    /// This function reads a JWT token from a cookie, extracts the account ID from the token, retrieves
+    /// user details from a database using the account ID, and stores the user details in the HttpContext
+    /// for use in downstream middleware or controllers.
+    /// </summary>
+    /// <param name="HttpContext">The HttpContext object represents the current HTTP request and response.
+    /// It contains information about the incoming request, such as headers, cookies, and query parameters,
+    /// as well as methods to manipulate the response.</param>
+
     public async Task InvokeAsync(HttpContext context)
     {
         var jwtCookie = context.Request.Cookies["Train"];
